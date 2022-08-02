d46757e3c38523dded2257ec22ab13127cc48435
Omkar Lolge <omkar.lolge119@gmail.com>
2022-08-02 16:44:45 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let input1 = document.getElementById("input");
+
+function myFunction() {
+    let input1 = document.getElementById("input");
+    input1.value = "16";
+    console.log(input1.value)
+  }
